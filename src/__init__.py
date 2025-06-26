5ef1f762b93279ebb9125b761134dee60f71abe9
haneric00 <ehrican@amazon.com>
2025-06-26 13:20:16 -0700

    mid implement of langchaininstrumentor

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 import logging
 from typing import Collection
 from opentelemetry.instrumentation.langchain.config import Config
@@ -21,77 +22,19 @@ from opentelemetry.instrumentation.langchain.callback_handler import (
     TraceloopCallbackHandler,
 )
 
-from opentelemetry.metrics import get_meter
-from opentelemetry.semconv_ai import Meters
-
-
-
-logger = logging.getLogger(__name__)
-
-_instruments = ("langchain-core > 0.1.0", )
-
+_instruments = ("langchain >= 0.1.0",)
 
 class LangChainInstrumentor(BaseInstrumentor):
-    """Main entry point for instrumentation"""
-    
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.handler = None
-        self.wrapped = None
-        # Config.exception_logger = exception_logger
-        # self.disable_trace_context_propagation = disable_trace_context_propagation
-
-
-    def instrumentation_dependencies(self) -> Collection[str]:
-        return _instruments
-
+  
+  
 
     def _instrument(self, **kwargs):
-        # Inject callback handler into LangChain
         # pass
         tracer_provider = kwargs.get("tracer_provider")
         tracer = get_tracer(__name__, __version__, tracer_provider)
-        
-        self.handler = TraceloopCallbackHandler(tracer_provider=tracer_provider)
-        
-        self._wrapped = [("langchain_core.callbacks", "BaseCallbackManager.__init__")]
-        
-        wrap_function_wrapper(
-            module="langchain_core.callbacks",
-            name="BaseCallbackManager.__init__",
-            wrapper=_BaseCallbackManagerInitWrapper(self.handler),
-        )
-        
-        # below code is for context propagation
-        
-        # if not self.disable_trace_context_propagation:
-        #     self._wrap_openai_functions_for_tracing(self.handler)
-    
-    def _uninstrument(self, **kwargs):
-        # Remove callback handler
-        
-        unwrap("langchain_core.callbacks", "BaseCallbackManager.__init__")
-        if hasattr(self, "_wrapped"):
-            for module, name in self._wrapped:
-                unwrap(module, name)
-        self.handler = None
-    
-    
-    
-class _BaseCallbackManagerInitWrapper:
-    def __init__(self, callback_manager: "TraceloopCallbackHandler"):
-        self._callback_manager = callback_manager
 
-    def __call__(
-        self,
-        wrapped,
-        instance,
-        args,
-        kwargs,
-    ) -> None:
-        wrapped(*args, **kwargs)
-        for handler in instance.inheritable_handlers:
-            if isinstance(handler, type(self._callback_manager)):
-                break
-        else:
-            instance.add_handler(self._callback_manager, True)
+        otelCallbackHandler = OpenTelemetryCallbackHandler(tracer)
+        traceloopCallbackHandler = TraceloopCallbackHandler(
+            tracer)
+    def _uninstrument(self, **kwargs):
+        pass
--- src/callback_handler.py
+++ src/callback_handler.py
@@ -1,6 +1,35 @@
-from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+import json
+import time
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union
+from langchain_core.callbacks import (
+    BaseCallbackHandler,
+)
 
-_instruments = ("langchain >= 0.1.0",)
+from opentelemetry.context.context import Context
+from opentelemetry.trace import SpanKind, set_span_in_context, Tracer
+from opentelemetry.trace.span import Span
+from opentelemetry.util.types import AttributeValue
+from uuid import UUID
 
-class LangChainInstrumentor(BaseInstrumentor):
-  pass
+
+@dataclass
+class SpanHolder:
+    span: Span
+    token: Any
+    context: Context
+    children: list[UUID]
+    workflow_name: str
+    entity_name: str
+    entity_path: str
+    start_time: float = field(default_factory=time.time)
+    request_model: Optional[str] = None
+    
+class TraceloopCallbackHandler(BaseCallbackHandler):
+    def __init__(
+        self, tracer: Tracer
+    ) -> None:
+        super().__init__()
+        self.tracer = tracer
+        self.spans: dict[UUID, SpanHolder] = {}
+        self.run_inline = True
