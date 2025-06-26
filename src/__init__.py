292c75684508b7e4137078bea60c1cf9002f22af
haneric00 <ehrican@amazon.com>
2025-06-26 14:41:06 -0700

    skeleton implementation of LangChainInstrumentor
    - added _instrument and _uninstrument
    - added accomodating classes for wrapping
    - added skeleton code for callbacks in callback_handler.py

--- src/__init__.py
+++ src/__init__.py
@@ -22,19 +22,47 @@ from opentelemetry.instrumentation.langchain.callback_handler import (
     TraceloopCallbackHandler,
 )
 
+from .callback_handler import OpenTelemetryCallbackHandler
+
+
+__all__ = ["OpenTelemetryCallbackHandler"]
+
 _instruments = ("langchain >= 0.1.0",)
 
 class LangChainInstrumentor(BaseInstrumentor):
-  
-  
-
     def _instrument(self, **kwargs):
-        # pass
         tracer_provider = kwargs.get("tracer_provider")
         tracer = get_tracer(__name__, __version__, tracer_provider)
 
         otelCallbackHandler = OpenTelemetryCallbackHandler(tracer)
-        traceloopCallbackHandler = TraceloopCallbackHandler(
-            tracer)
+        
+        wrap_function_wrapper(
+            module="langchain_core.callbacks",
+            name="BaseCallbackManager.__init__",
+            wrapper=_BaseCallbackManagerInitWrapper(otelCallbackHandler),
+        )
     def _uninstrument(self, **kwargs):
-        pass
+        unwrap("langchain_core.callbacks", "BaseCallbackManager.__init__")
+        if hasattr(self, "_wrapped"):
+            for module, name in self._wrapped:
+                unwrap(module, name)
+        self.handler = None
+    
+    
+class _BaseCallbackManagerInitWrapper:
+    def __init__(self, callback_manager: "OpenTelemetryCallbackHandler"):
+        self._callback_manager = callback_manager
+
+    def __call__(
+        self,
+        wrapped,
+        instance,
+        args,
+        kwargs,
+    ) -> None:
+        wrapped(*args, **kwargs)
+        for handler in instance.inheritable_handlers:
+            if isinstance(handler, type(self._callback_manager)):
+                break
+        else:
+            instance.add_handler(self._callback_manager, True)
---
+++ src/span_attributes.py
@@ -0,0 +1,1 @@
+# file for attribute constants (span attributes)
