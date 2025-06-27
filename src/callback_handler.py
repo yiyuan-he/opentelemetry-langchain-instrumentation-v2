158d8c207546236f9df5f448b4480b49e85863c6
haneric00 <ehrican@amazon.com>
2025-06-27 11:42:13 -0700

    updated callbacks to include the main callbacks specifically listed in the design doc + the parameters they are listed with

--- src/callback_handler.py
+++ src/callback_handler.py
@@ -38,35 +38,44 @@ class OpenTelemetryCallbackHandler(BaseCallbackHandler):
         self.spans: dict[UUID, SpanHolder] = {}
         
     
-    def on_chat_model_start(self, serialized, messages, **kwargs):
+    def on_chat_model_start(self, serialized, messages, run_id, parent_run_id, **kwargs):
         pass
     
-    def on_llm_start(self, serialized, prompts, **kwargs):
+    def on_llm_start(self, serialized, prompts, run_id, parent_run_id, **kwargs):
         pass
     
-    def on_llm_new_token(self, token, **kwargs):
+    # def on_llm_new_token(self, token, **kwargs):
+    #     pass
+    
+    def on_llm_end(self, response, run_id, parent_run_id, **kwargs):
         pass
     
-    def on_llm_end(self, response, **kwargs):
-        pass
-    
-    def on_llm_error(self, error, **kwargs):
+    def on_llm_error(self, error, run_id, parent_run_id, **kwargs):
         pass
 
-    def on_chain_start(self, serialized, inputs, **kwargs):
+    def on_chain_start(self, serialized, inputs, run_id, parent_run_id, **kwargs):
         pass 
     
-    def on_chain_end(self, outputs, **kwargs):   
+    def on_chain_end(self, outputs, run_id, parent_run_id, **kwargs):   
         pass
     
     def on_chain_error(self, error, run_id, parent_run_id, tags, **kwargs):
         pass
     
-    def on_tool_start(self, serialized, input_str, **kwargs):
+    def on_tool_start(self, serialized, input_str, run_id, parent_run_id, **kwargs):
         pass
     
-    def on_tool_end(self, output, **kwargs):
+    def on_tool_end(self, output, run_id, parent_run_id, **kwargs):
         pass
     
-    def on_tool_error(self, error, **kwargs):
+    def on_tool_error(self, error, run_id, parent_run_id, **kwargs):
+        pass
+    
+    def on_agent_action(self, action, run_id, parent_run_idone, **kwargs):
+        pass
+    
+    def on_agent_finish(self, finish, run_id, parent_run_id, **kwargs):
+        pass
+
+    def on_agent_error(self, error, run_id, parent_run_id, **kwargs):
         pass
