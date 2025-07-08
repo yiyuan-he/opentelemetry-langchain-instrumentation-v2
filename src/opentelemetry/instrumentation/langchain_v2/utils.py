8735125ab230723102303f023f66092c84e77c1f
haneric00 <ehrican@amazon.com>
2025-07-08 14:14:35 -0700

    delete unused helper in utils

--- src/opentelemetry/instrumentation/langchain_v2/utils.py
+++ src/opentelemetry/instrumentation/langchain_v2/utils.py
@@ -11,33 +11,6 @@ from opentelemetry.instrumentation.langchain.config import Config
 from pydantic import BaseModel
 
 
-class CallbackFilteredJSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, dict):
-            if "callbacks" in o:
-                del o["callbacks"]
-                return o
-
-        if dataclasses.is_dataclass(o):
-            return dataclasses.asdict(o)
-
-        if hasattr(o, "to_json"):
-            return o.to_json()
-
-        if isinstance(o, BaseModel) and hasattr(o, "model_dump_json"):
-            return o.model_dump_json()
-
-        if isinstance(o, datetime.datetime):
-            return o.isoformat()
-
-        try:
-            return str(o)
-        except Exception:
-            logger = logging.getLogger(__name__)
-            logger.debug("Failed to serialize object of type: %s", type(o).__name__)
-            return ""
-
-
 def dont_throw(func):
     """
     A decorator that wraps the passed in function and logs exceptions instead of throwing them.
