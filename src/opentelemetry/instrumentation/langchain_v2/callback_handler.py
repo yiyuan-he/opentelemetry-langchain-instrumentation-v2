487b1f0916f3414fa7c5af4b9a5c5d786566b7f8
haneric00 <ehrican@amazon.com>
2025-07-08 14:39:35 -0700

    delete unused import

--- src/opentelemetry/instrumentation/langchain_v2/callback_handler.py
+++ src/opentelemetry/instrumentation/langchain_v2/callback_handler.py
@@ -19,7 +19,7 @@ from opentelemetry.instrumentation.utils import _SUPPRESS_INSTRUMENTATION_KEY
 
 
 from opentelemetry.instrumentation.langchain_v2.span_attributes import Span_Attributes, GenAIOperationValues
-from opentelemetry.instrumentation.langchain_v2.utils import dont_throw, CallbackFilteredJSONEncoder
+from opentelemetry.instrumentation.langchain_v2.utils import dont_throw
 from opentelemetry.trace.status import Status, StatusCode
 
 # below dataclass stolen from openLLMetry
