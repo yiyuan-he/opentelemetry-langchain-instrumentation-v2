3b0a53724fbf198da5f65fda646488756c94c308
haneric00 <ehrican@amazon.com>
2025-07-07 17:15:32 -0700

    agent callback handlers written

--- src/opentelemetry/instrumentation/langchain_v2/callback_handler.py
+++ src/opentelemetry/instrumentation/langchain_v2/callback_handler.py
@@ -361,6 +361,16 @@ class OpenTelemetryCallbackHandler(BaseCallbackHandler):
                      **kwargs: Any
                      ):   
         
+        print("ON CHAIN END")
+        print(f"Chain outputs: {outputs}")
+        print(f"Run ID: {run_id}")
+        print(f"Parent Run ID: {parent_run_id}")
+        print(f"Tags: {tags}")
+        print(f"Additional kwargs: {kwargs}")
+        
+        
+        
+    
         if context_api.get_value(_SUPPRESS_INSTRUMENTATION_KEY):
             return
 
@@ -464,6 +474,20 @@ class OpenTelemetryCallbackHandler(BaseCallbackHandler):
                     ):
         tool = getattr(action, "tool", None)
         tool_input = getattr(action, "tool_input", None)
+        log = getattr(action, "log", None)
+        # print("ON AGENT ACTION")
+        # print(f"Agent Action:")
+        # print(f"  Tool: {tool}")
+        # print(f"  Tool Input: {tool_input}")
+        # print(f"  Log: {log}")
+        # # Print run IDs
+        # print(f"Run ID: {run_id}")
+        # print(f"Parent Run ID: {parent_run_id}")
+        # # Print any additional kwargs
+        # print("Additional kwargs:")
+        # for key, value in kwargs.items():
+        #     print(f"  {key}: {value}")
+        
         name = action.tool
         span_name = f"{name}.{SpanKind.INTERNAL}"
         span = self._create_span(
@@ -484,6 +508,13 @@ class OpenTelemetryCallbackHandler(BaseCallbackHandler):
                         parent_run_id: UUID, 
                         **kwargs: Any
                         ):
+        # print("ON AGENT FINISH")
+        # print(f"Agent finish return values: {finish.return_values}")
+        # print(f"Agent finish log: {finish.log}")
+        # print(f"Run ID: {run_id}")
+        # print(f"Parent Run ID: {parent_run_id}")
+        # print(f"Additional kwargs: {kwargs}")
+        
         span = self.span_mapping[run_id].span
         
         _set_span_attribute(span, "agent.tool.output", finish.return_values['output'])
